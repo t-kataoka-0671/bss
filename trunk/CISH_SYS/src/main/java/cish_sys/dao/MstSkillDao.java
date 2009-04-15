e022138bbf4bf0646c1dcc8c98bd47b4364f2451
t.kataoka.0671 <t.kataoka.0671@1dd8ad76-2856-11de-9600-dfb65e630a92>
2009-04-15 10:52:49 +0000

    検索＆ページ機能追加

--- trunk/CISH_SYS/src/main/java/cish_sys/dao/MstSkillDao.java
+++ trunk/CISH_SYS/src/main/java/cish_sys/dao/MstSkillDao.java
@@ -3,18 +3,21 @@ package cish_sys.dao;
 import org.seasar.dao.annotation.tiger.Arguments;
 import org.seasar.dao.annotation.tiger.S2Dao;
 import cish_sys.entity.MstSkill;
+import cish_sys.paging.MstSkillPagerCondition;
 
 @S2Dao(bean=MstSkill.class)
 public interface MstSkillDao {
 	public MstSkill[] selectAll();
 
-	
+	@Arguments({"argSkillCode","argSkillDiv"})
+	public MstSkill[] findBySkillCodeAndSkillDivPagerCondition(java.lang.String argSkillCode, java.lang.String argSkillDiv, MstSkillPagerCondition dto);
+
 	@Arguments({"SKILL_DIV","SKILL_CODE"})
 	public MstSkill selectById(String skillDiv,String skillCode);
-	
+
 	public int insert(MstSkill mstSkill);
 
 	public int update(MstSkill mstSkill);
-	
+
 	public int delete(MstSkill mstSkill);
 }
