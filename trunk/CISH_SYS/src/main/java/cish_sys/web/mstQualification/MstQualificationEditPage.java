15698bd9ab04cee1d5bbaf917f2bb9198af82a37
sako@cis-net.co.jp <sako@cis-net.co.jp@1dd8ad76-2856-11de-9600-dfb65e630a92>
2009-06-03 09:32:37 +0000

    マスタメンテナンス【資格マスタ】画面レイアウト、項目の日本語化、バリデーションチェックの修正

--- trunk/CISH_SYS/src/main/java/cish_sys/web/mstQualification/MstQualificationEditPage.java
+++ trunk/CISH_SYS/src/main/java/cish_sys/web/mstQualification/MstQualificationEditPage.java
@@ -2,6 +2,7 @@ package cish_sys.web.mstQualification;
 
 import java.util.Date;
 
+import org.seasar.teeda.extension.annotation.validator.ByteLength;
 import org.seasar.teeda.extension.annotation.validator.Required;
 
 import org.seasar.teeda.core.exception.AppFacesException;
@@ -31,20 +32,52 @@ public class MstQualificationEditPage extends AbstractMstQualificationPage {
 
 	@Override
 	@Required
+	@ByteLength(maximum = 2)
 	public void setQuaDiv(String quaDiv) {
 		super.setQuaDiv(quaDiv);
 	}
 
 	@Override
 	@Required
+	@ByteLength(maximum = 3)
 	public void setQuaCode(String quaCode) {
 		super.setQuaCode(quaCode);
 	}
 
 	@Override
+	@ByteLength(maximum = 50)
+	public void setQuaName(String quaName) {
+		super.setQuaName(quaName);
+	}
+
+	@Override
+	@ByteLength(maximum = 8)
+	public void setInsUser(String insUser) {
+		super.setInsUser(insUser);
+	}
+
+	@Override
+	@ByteLength(maximum = 8)
+	public void setUpdUser(String updUser) {
+		super.setUpdUser(updUser);
+	}
+
+	@Override
 	@Required
+	@ByteLength(maximum = 1)
 	public void setDelFlag(String delflag) {
 		super.setDelFlag(delflag);
+	}
+
+	public boolean isCreate(){
+		Date sysDate = new Date();
+		if(getCrudType() == CrudType.CREATE) {
+			super.setInsDate(sysDate);
+			return true;
+		}else{
+			super.setUpdDate(sysDate);
+			return false;
+		}
 	}
 
 	public String getIsNotCreateStyle() {
